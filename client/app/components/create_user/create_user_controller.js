6d1c1a7c525beeb55044231786c4e2aff532b973
Cooper Benson <skycoop@gmail.com>
2016-01-21 22:08:36 -0700

    Better form updating. Now has password confirmation :D

--- client/app/components/create_user/create_user_controller.js
+++ client/app/components/create_user/create_user_controller.js
@@ -1,64 +1,85 @@
 angular.module('app').controller('createUserController', [
-    '$location',
-    '$rootScope',
-    '$scope',
-    'Person',
-    function ($location, $rootScope, $scope, Person) {
+  '$location',
+  '$rootScope',
+  '$scope',
+  'Person',
+  function($location, $rootScope, $scope, Person) {
 
+    $scope.credentials = {};
+    $scope.confirmationModel = {};
 
-        $scope.credentials = {};
+    $scope.credentialsFields = [
+      {
+        key: 'firstName',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'text',
+          label: 'First Name',
+          placeholder: 'Enter your first name',
+          required: true
+        }
+      },
+      {
+        key: 'lastName',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'text',
+          label: 'Last Name',
+          placeholder: 'Enter your last name',
+          required: true
+        }
+      },
+      {
+        key: 'email',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'email',
+          label: 'Email address',
+          placeholder: 'Enter email',
+          required: true
+        }
+      },
+      {
+        key: 'password',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'password',
+          label: 'Password',
+          required: true,
+          minlength: 5
+        }
+      },
+      {
+        key: 'confirmPassword',
+        type: 'input',
+        optionsTypes: ['matchField'],
+        model: $scope.confirmationModel,
+        templateOptions: {
+          type: 'password',
+          label: 'Password Confirmation',
+          required: true,
+          minlength: 5
+        },
+        data: {
+          fieldToMatch: 'password',
+          modelToMatch: $scope.credentials
+        }
+      }
+    ];
 
-        $scope.credentialsFields = [
-            {
-                key: "firstName",
-                type: "input",
-                templateOptions: {
-                    type: 'text',
-                    label: 'First Name',
-                    placeholder: 'Enter your first name',
-                    required: true
-                }
-            },
-            {
-                key: "lastName",
-                type: "input",
-                templateOptions: {
-                    type: 'text',
-                    label: 'Last Name',
-                    placeholder: 'Enter your last name',
-                    required: true
-                }
-            },
-            {
-                key: "email",
-                type: "input",
-                templateOptions: {
-                    type: 'email',
-                    label: 'Email address',
-                    placeholder: 'Enter email',
-                    required: true
-                }
-            },
-            {
-                key: "password",
-                type: "input",
-                templateOptions: {
-                    type: "password",
-                    label: "Password",
-                    required: true,
-                    minlength: 5
-                }
-            }
-        ];
-
-        $scope.onSubmit = function () {
-            $scope.credentials.email =
-                $scope.credentials.email.toLowerCase();
-            Person.create($scope.credentials, function (result) {
-                //$location.path('/club');
-            }, function (error) {
-                $scope.error = 'Woops, something isn\'t right';
-            });
-        };
-    }
+    $scope.onSubmit = function() {
+      $scope.credentials.email =
+          $scope.credentials.email.toLowerCase();
+      Person.create($scope.credentials, function(result) {
+        //$location.path('/club');
+      }, function(error) {
+        $scope.error = 'Woops, something isn\'t right';
+      });
+      alert(JSON.stringify($scope.credentials));
+    };
+  }
 ]);
