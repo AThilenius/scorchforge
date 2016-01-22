f3a3d049f267ceb51d0a76ab376be1c851966ace
Cooper Benson <skycoop@gmail.com>
2016-01-21 22:08:36 -0700

    Better form updating. Now has password confirmation :D

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -5,6 +5,7 @@ var app = angular.module('app', [
   'ngAnimate',
   'ngCookies',
   'ngMaterial',
+  'ngMessages',
   'dndLists',
   'ui.ace',
   'ui.router',
@@ -13,7 +14,9 @@ var app = angular.module('app', [
   'xeditable',
   'thilenius.navbar',
   'thilenius.sidebar',
-  'thilenius.content_window'
+  'thilenius.content_window',
+  'formly',
+  'formlyBootstrap'
 ]);
 
 app.run([
@@ -46,3 +49,81 @@ app.config([
     ]);
   }
 ]);
+
+app.run(function(formlyConfig, formlyValidationMessages, formlyApiCheck) {
+  formlyConfig.setWrapper({
+    name: 'validation',
+    types: ['input', 'customInput'],
+    templateUrl: 'my-messages.html'
+  });
+
+  formlyValidationMessages.addStringMessage('required', 'This field is required');
+  formlyValidationMessages.addStringMessage('email', 'Email is invalid');
+  formlyValidationMessages.addStringMessage('minlength', 'Too short');
+
+  formlyConfig.setType({
+    name: 'customInput',
+    extends: 'input',
+    apiCheck: function(check) {
+      return {
+        templateOptions: {
+          foo: check.string.optional
+        }
+      };
+    }
+  });
+
+  formlyConfig.setType({
+    name: 'matchField',
+    apiCheck: function() {
+      return {
+        data: {
+          fieldToMatch: formlyExampleApiCheck.string
+        }
+      };
+    },
+    apiCheckOptions: {
+      prefix: 'matchField type'
+    },
+    defaultOptions: function matchFieldDefaultOptions(options) {
+      return {
+        extras: {
+          validateOnModelChange: true
+        },
+        expressionProperties: {
+          'templateOptions.disabled': function(viewValue, modelValue, scope) {
+            var matchField = find(scope.fields, 'key', options.data.fieldToMatch);
+            if (!matchField) {
+              throw new Error('Could not find a field for the key ' + options.data.fieldToMatch);
+            }
+            var model = options.data.modelToMatch || scope.model;
+            var originalValue = model[options.data.fieldToMatch];
+            var invalidOriginal = matchField.formControl && matchField.formControl.$invalid;
+            return !originalValue || invalidOriginal;
+          }
+        },
+        validators: {
+          fieldMatch: {
+            expression: function(viewValue, modelValue, fieldScope) {
+              var value = modelValue || viewValue;
+              var model = options.data.modelToMatch || fieldScope.model;
+              return value === model[options.data.fieldToMatch];
+            },
+            message: options.data.matchFieldMessage || '"Must match"'
+          }
+        }
+      };
+
+      function find(array, prop, value) {
+        var foundItem;
+        array.some(function(item) {
+          if (item[prop] === value) {
+            foundItem = item;
+          }
+          return !!foundItem;
+        });
+        return foundItem;
+      }
+    }
+  });
+});
--- client/app/components/create_user/create_user_controller.js
+++ client/app/components/create_user/create_user_controller.js
@@ -1,18 +1,85 @@
 angular.module('app').controller('createUserController', [
-    '$location',
-    '$rootScope',
-    '$scope',
-    'Person',
-    function ($location, $rootScope, $scope, Person) {
+  '$location',
+  '$rootScope',
+  '$scope',
+  'Person',
+  function($location, $rootScope, $scope, Person) {
 
-        $scope.createAccount = function () {
-            $scope.createCredentials.email =
-                $scope.createCredentials.email.toLowerCase();
-            Person.create($scope.createCredentials, function (result) {
-                //$location.path('/club');
-            }, function (error) {
-                $scope.error = 'Woops, something isn\'t right';
-            });
-        };
-    }
+    $scope.credentials = {};
+    $scope.confirmationModel = {};
+
+    $scope.credentialsFields = [
+      {
+        key: 'firstName',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'text',
+          label: 'First Name',
+          placeholder: 'Enter your first name',
+          required: true
+        }
+      },
+      {
+        key: 'lastName',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'text',
+          label: 'Last Name',
+          placeholder: 'Enter your last name',
+          required: true
+        }
+      },
+      {
+        key: 'email',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'email',
+          label: 'Email address',
+          placeholder: 'Enter email',
+          required: true
+        }
+      },
+      {
+        key: 'password',
+        type: 'input',
+        model: $scope.credentials,
+        templateOptions: {
+          type: 'password',
+          label: 'Password',
+          required: true,
+          minlength: 5
+        }
+      },
+      {
+        key: 'confirmPassword',
+        type: 'input',
+        optionsTypes: ['matchField'],
+        model: $scope.confirmationModel,
+        templateOptions: {
+          type: 'password',
+          label: 'Password Confirmation',
+          required: true,
+          minlength: 5
+        },
+        data: {
+          fieldToMatch: 'password',
+          modelToMatch: $scope.credentials
+        }
+      }
+    ];
+
+    $scope.onSubmit = function() {
+      $scope.credentials.email =
+          $scope.credentials.email.toLowerCase();
+      Person.create($scope.credentials, function(result) {
+        //$location.path('/club');
+      }, function(error) {
+        $scope.error = 'Woops, something isn\'t right';
+      });
+      alert(JSON.stringify($scope.credentials));
+    };
+  }
 ]);
