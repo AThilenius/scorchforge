576ec53bc5a63206f41e71392682585ac346903b
Cooper Benson <skycoop@gmail.com>
2016-01-25 16:11:58 -0700

    Generaling the update_deps script (also renaming .sh since it's a shell script)

--- client/update_deps.sh
+++ client/update_deps.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 
-mkdir --parent /root/thilenius/scorch/client/gen-files
+mkdir --parent gen-files
 
 TIMESTAMP=$(date +%y%m%d.%H.%M%S)
 
 function GlobJsToIndex {
   find $1 -name *.js | while read line; do \
-    echo "<script src=\"$line\"></script>" >> index.html; \
+    echo "    <script src=\"$line\"></script>" >> index.html; \
   done
+  echo "" >> index.html
 }
 
 function GlobCssToIndex {
   find $1 -name *.css | while read line; do \
-    echo "<link rel=\"stylesheet\" href=\"$line\" />" >> index.html; \
+    echo "    <link rel=\"stylesheet\" href=\"$line\" />" >> index.html; \
   done
+  echo "" >> index.html
 }
 
-echo 'Createing a development index.html file'
-pushd /root/thilenius/scorch/client
-rm -rf /root/thilenius/scorch/client/index.html
+echo 'Creating a development index.html file'
+rm -rf index.html
 cat << EOM >> index.html
 <!doctype html>
 <html ng-app="app" ng-controller="loginController"
       class="{{\$root.htmlClass ? \$root.htmlClass : 'default-body'}}">
 <head><title ng-bind="\$root.title || 'Scorch'"></title>
-<link rel="shortcut icon" href="assets/flame-512.png" />
-<link href="build/_bower.css" rel="stylesheet">
-<script src="build/compiled_libs.js"></script>
+    <link rel="shortcut icon" href="assets/flame-512.png" />
+    <link href="build/_bower.css" rel="stylesheet">
+
+    <script src="build/compiled_libs.js"></script>
 EOM
-echo "<script>var FORGE_VERSION=\"${TIMESTAMP}\";</script>" >> index.html
+echo "    <script>var FORGE_VERSION=\"${TIMESTAMP}\";</script>" >> index.html
+echo "" >> index.html
 
 GlobCssToIndex app
 GlobJsToIndex app
 
 cat << EOM >> index.html
+    <base href="/">
 </head>
 <body class="{{\$root.bodyClass ? \$root.bodyClass : 'default-body'}}">
 <div ui-view></div>
 </body></html>
 EOM
-popd
+
