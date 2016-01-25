9936baa28395aa54377e6cdffb2b8482bcd8d57b
Cooper Benson <skycoop@gmail.com>
2016-01-25 12:34:41 -0700

    Now has modern updating (only for HTML 5, but Angular will fallback correctly if running an older browser)

--- client/app/app.route.js
+++ client/app/app.route.js
@@ -4,24 +4,27 @@
 angular.module('app').config([
   '$stateProvider',
   '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+  '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider.state('login',
-                         {
-                           url: '/login',
-                           templateUrl: 'app/components/login/login.html',
-                           controller: 'loginController'
-                         })
+        {
+          url: '/login',
+          templateUrl: 'app/components/login/login.html',
+          controller: 'loginController'
+        })
         .state('create_user',
-               {
-                 url: '/create_user',
-                 templateUrl: 'app/components/create_user/create_user.html',
-                 controller: 'createUserController'
-               })
+            {
+              url: '/create_user',
+              templateUrl: 'app/components/create_user/create_user.html',
+              controller: 'createUserController'
+            })
         .state('forge', {
           url: '/forge',
           templateUrl: 'app/components/forge/forge.html',
           controller: 'forgeController'
         });
     $urlRouterProvider.otherwise('forge');
+
+    $locationProvider.html5Mode(true);
   }
 ]);
