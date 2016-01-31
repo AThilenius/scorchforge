1581cb2d3d8bc1248dfb07b9617d0250caee4242
Alec Thilenius <alec@thilenius.com>
2016-01-31 11:03:44 -0700

    First draft implementation of term.js, talking with pty.js via Socket.io. For now, the pty is hosted directly (not in a docker container) and only supports a single shell.

--- client/app/base/string.js
+++ client/app/base/string.js
@@ -30,3 +30,14 @@ var newGuid = newUuid = function() {
     return v.toString(16);
   });
 };
+
+var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var ID_LENGTH = 8;
+
+var newShortGuid = newShortUuid = function() {
+  var rtn = '';
+  for (var i = 0; i < ID_LENGTH; i++) {
+    rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+  }
+  return rtn;
+};
